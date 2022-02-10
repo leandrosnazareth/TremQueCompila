953fe07255a8fe748027bb256ed9eaeedac6b59b
Leandro Nazareth <52218718+leandrosnazareth@users.noreply.github.com>
2022-02-10 08:30:05 -0300

    Compilando, falta pegar dados do arquivo

---
+++ Imprimir.java
@@ -0,0 +1,16 @@
+
+/**
+ *
+ * @author leand
+ */
+public class Imprimir {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Imprimindo");
+    }
+    
+}
---
+++ src/main/java/br/com/blsoft/tremquecompila/Imprimir.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package br.com.blsoft.tremquecompila;
+
+/**
+ *
+ * @author leand
+ */
+public class Imprimir {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Imprimindo");
+    }
+    
+}
